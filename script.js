ff857f0d62b3ab3d47423a38bad12c190bb50917
Snoppei <negulyaev2017@gmail.com>
2024-10-18 16:45:02 +0300

    add: feature cards, fix: transition in toggle switch

--- script.js
+++ script.js
@@ -2,11 +2,4 @@ let firstItemInSwitch = document.querySelector('.switch-btn__content_first');
 let secondItemInSwitch = document.querySelector('.switch-btn__content_second');
 $('.switch-btn').click(function () {
     $(this).toggleClass('switch-on');
-    if ($(this).hasClass('switch-on')) {
-        firstItemInSwitch.style.color = '#F1F1F1'; /*докрутить сюда таймаут какой-нибудь */
-        secondItemInSwitch.style.color = '#151515';
-    } else {
-        firstItemInSwitch.style.color = '#151515';
-        secondItemInSwitch.style.color = '#F1F1F1';
-    }
 });
