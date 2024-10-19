830e5382251fe745d69c13900b391fce2b5644af
Snoppei <negulyaev2017@gmail.com>
2024-10-19 04:26:26 +0300

    add: features-images in hero section

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ let secondItemInSwitch = document.querySelector('.switch-btn__content_second');
 $('.switch-btn').click(function () {
     $(this).toggleClass('switch-on');
 });
+
+$('.features-card_first').hover(function(){
+    $('.features-image_current').css("background-image", "url(assets/speedModes.png)");
+    $('.features-image_next').css("background-image", "url(assets/batteryRecoverySystem.png)");
+});
+
+$('.features-card_second').hover(function(){
+    $('.features-image_current').css("background-image", "url(assets/batteryRecoverySystem.png)");
+    $('.features-image_next').css("background-image", "url(assets/highCapacityBattery.png)");
+});
+
+$('.features-card_third').hover(function(){ // в гугле такая конструкция работает стремно, а в мозилле вообще не работает...
+    $('.features-image_current').css("background-image", "url(assets/highCapacityBattery.png)");
+    $('.features-image_next').css("background-image", "url(assets/speedModes.png)");
+});
