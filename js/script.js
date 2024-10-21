4feae7c733203ca76465c6e73e498a24a8303d8a
Snoppei <44434924+Snoppei@users.noreply.github.com>
2024-10-22 02:30:41 +0300

    add: safety section

--- js/script.js
+++ js/script.js
@@ -1,36 +1,151 @@
-let firstItemInSwitch = document.querySelector('.switch-btn__content_first');
-let secondItemInSwitch = document.querySelector('.switch-btn__content_second');
-$('.switch-btn').click(function () {
-    $(this).toggleClass('switch-on');
+const classesAndUrls = [
+    [
+        '.features-point_first',
+        './assets/speedModes.png',
+        './assets/batteryRecoverySystem.png',
+        'feature'
+    ],
+    [
+        '.features-point_second',
+        './assets/batteryRecoverySystem.png',
+        './assets/highCapacityBattery.png',
+        'feature'
+    ],
+    [
+        '.features-point_third',
+        './assets/highCapacityBattery.png',
+        './assets/speedModes.png',
+        'feature'
+    ],
+    [
+        '.safety-point_first',
+        './assets/safety-first.png',
+        './assets/safety-second.png',
+        'safety'
+    ],
+    [
+        '.safety-point_second',
+        './assets/safety-second.png',
+        './assets/safety-third.png',
+        'safety'
+    ],
+    [
+        '.safety-point_third',
+        './assets/safety-third.png',
+        './assets/safety-fourth.png',
+        'safety'
+    ],
+    [
+        '.safety-point_fourth',
+        './assets/safety-fourth.png',
+        './assets/safety-first.png',
+        'safety'
+    ]
+]
+
+// const imageUrls = [
+//     './assets/speedModes.png',
+//     './assets/batteryRecoverySystem.png',
+//     './assets/highCapacityBattery.png',
+//     './assets/safety-first.png',
+//     './assets/safety-second.png',
+//     './assets/safety-third.png',
+//     './assets/safety-fourth.png'
+// ];
+$(document).ready(function () {
+
+    let firstItemInSwitch = document.querySelector('.switch-btn__content_first');
+    let secondItemInSwitch = document.querySelector('.switch-btn__content_second');
+    $('.switch-btn').click(function () {
+        $(this).toggleClass('switch-on');
+    });
+
+    $(`${classesAndUrls[0][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[0][1], classesAndUrls[0][2], classesAndUrls[0][3]);
+    });
+
+    $(`${classesAndUrls[1][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[1][1], classesAndUrls[1][2], classesAndUrls[0][3]);
+    });
+
+    $(`${classesAndUrls[2][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[2][1], classesAndUrls[2][2], classesAndUrls[0][3]);
+    });
+
+    $(`${classesAndUrls[3][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[3][1], classesAndUrls[3][2], classesAndUrls[3][3]);
+    });
+
+    $(`${classesAndUrls[4][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[4][1], classesAndUrls[4][2], classesAndUrls[4][3]);
+    });
+
+    $(`${classesAndUrls[5][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[5][1], classesAndUrls[5][2], classesAndUrls[5][3]);
+    });
+
+    $(`${classesAndUrls[6][0]}`).click(function () {
+        smootlyChangeFeaturesCardsBG(classesAndUrls[6][1], classesAndUrls[6][2], classesAndUrls[6][3]);
+    });
+
 });
 
-const imageUrls = [
-    './assets/speedModes.png',
-    './assets/batteryRecoverySystem.png',
-    './assets/highCapacityBattery.png'
-];
-
-$('.features-card_first').click(function () {
-    smootlyChangeFeaturesCardsBG(imageUrls[0], imageUrls[1]);
-});
-
-$('.features-card_second').click(function () {
-    smootlyChangeFeaturesCardsBG(imageUrls[1], imageUrls[2]);
-});
-
-$('.features-card_third').click(function () {
-    smootlyChangeFeaturesCardsBG(imageUrls[2], imageUrls[0]);
-});
-
-function smootlyChangeFeaturesCardsBG(imgUrlToChangeCurrent, imgUrlToChangeNext) {
-    $('.features-image_current').css('opacity', '0');
-    $('.features-image_next').css('opacity', '0');
-    setTimeout(function () {
-        $('.features-image_current').css("background-image", `url(${imgUrlToChangeCurrent})`);
-        $('.features-image_next').css("background-image", `url(${imgUrlToChangeNext})`);
-    }, 300);
-    setTimeout(function () {
-        $('.features-image_current').css('opacity', '1');
-        $('.features-image_next').css('opacity', '1');
-    }, 500);
+function smootlyChangeFeaturesCardsBG(imgUrlToChangeCurrent, imgUrlToChangeNext, section) {
+    if (section == 'feature') {
+        $('.features-image_current').css('opacity', '0');
+        $('.features-image_next').css('opacity', '0');
+        setTimeout(function () {
+            $('.features-image_current').css("background-image", `url(${imgUrlToChangeCurrent})`);
+            $('.features-image_next').css("background-image", `url(${imgUrlToChangeNext})`);
+        }, 300);
+        setTimeout(function () {
+            $('.features-image_current').css('opacity', '1');
+            $('.features-image_next').css('opacity', '1');
+        }, 500);
+    } else {
+        $('.safety-image_current').css('opacity', '0');
+        $('.safety-image_next').css('opacity', '0');
+        setTimeout(function () {
+            $('.safety-image_current').css("background-image", `url(${imgUrlToChangeCurrent})`);
+            $('.safety-image_next').css("background-image", `url(${imgUrlToChangeNext})`);
+            if(imgUrlToChangeCurrent == "./assets/safety-fourth.png") {
+                $('.safety-image_next').css('background-position', 'right');
+                $('.safety-image_current').css('background-position', 'center');
+            } else {
+                $('.safety-image_next').css('background-position', 'center');
+                $('.safety-image_current').css('background-position', 'right');
+            }
+        }, 300);
+        setTimeout(function () {
+            $('.safety-image_current').css('opacity', '1');
+            $('.safety-image_next').css('opacity', '1');
+        }, 500);
+    }
 }
+
+
+// function repositionBG(current, next, imgUrlToChangeCurrent, section) {
+//     if (section == 'safety') {
+//         switch (imgUrlToChangeCurrent) {
+//             case './assets/safety-first.png':
+//                 next.css('background-position', 'center');
+//                 current.css('background-position', 'right');
+//                 break;
+//             case './assets/safety-second':
+//                 next.css('background-position', 'center');
+//                 current.css('background-position', 'center');
+//                 break;
+//             case './assets/safety-third':
+//                 next.css('background-position', 'center');
+//                 current.css('background-position', 'right');
+//                 break;
+//             case './assets/safety-fourth':
+//                 next.css('background-position', 'right');
+//                 current.css('background-position', 'center');
+//                 break;
+
+//         }
+//     } else {
+//         return 0;
+//     }
+// }
